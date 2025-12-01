697ee521d0e735d981160a5d8bb02db1df4f8c9e
Matheus-Dev-java <matheus.dev.back23@gmail.com>
2025-11-30 23:19:24 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    TWILIO_ACCOUNT_SID: process.env.AC94c7baafaf78a1951ab2427e004310e3,
-    TWILIO_AUTH_TOKEN: process.env.184adf49a4ad8e5dc987e019aa930edf,
-    TWILIO_NUMBER: process.env.+1415523-8886,
-    ADMIN_WHATSAPP: process.env.557588669207,
-    DATABASE_URL: process.env.postgresql://postgres.uaasrealnqocaynryvlg:theuzin123@aws-0-sa-east-1.pooler.supabase.com:6543/postgres,
-    BASE_URL: process.env.https://milly-nails-ak7y.vercel.app
+    TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+    TWILIO_NUMBER: process.env.TWILIO_NUMBER,
+    ADMIN_WHATSAPP: process.env.ADMIN_WHATSAPP,
+    DATABASE_URL: process.env.DATABASE_URL,
+    BASE_URL: process.env.BASE_URL,
   },
 }
 
